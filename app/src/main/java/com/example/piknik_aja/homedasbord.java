9fe4cbed95349f2bc98b519fe76a4062771f4d6c
AnggiAprianda <anggiaprianda2000@gmail.com>
2021-09-28 11:57:24 +0700

    Commit Awal

---
+++ app/src/main/java/com/example/piknik_aja/homedasbord.java
@@ -0,0 +1,14 @@
+package com.example.piknik_aja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homedasbord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homedasbord);
+    }
+}
---
+++ app/src/main/java/views/SplashActivity.java
@@ -0,0 +1,16 @@
+package views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.piknik_aja.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
