34adc3b91704eb698611b30d98336b825cae8d4c
RafikaFadilaDaulay <rafikafadila15@gmail.com>
2021-09-30 02:41:56 +0700

    Penambahan Tampilan Akun Saya, Halaman Notifikasi dan Halaman Keberangkatan

---
+++ app/src/main/java/com/example/piknik_aja/TampilanAkunSaya.java
@@ -0,0 +1,14 @@
+package com.example.piknik_aja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TampilanAkunSaya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_akun_saya);
+    }
+}
---
+++ app/src/main/java/com/example/piknik_aja/TampilanKeberangkatan.java
@@ -0,0 +1,14 @@
+package com.example.piknik_aja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TampilanKeberangkatan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_keberangkatan);
+    }
+}
